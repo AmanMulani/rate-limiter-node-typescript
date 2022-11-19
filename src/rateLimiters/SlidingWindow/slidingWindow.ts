33d8525ad7b1201ac0ac2de668e226d60064188c
Aman Mulani <amanmulani369@gmail.com>
2022-11-19 23:56:01 +0530

    WIP | AM | Sliding Window Rate Limiter

--- src/rateLimiters/SlidingWindow/slidingWindow.ts
+++ src/rateLimiters/SlidingWindow/slidingWindow.ts
@@ -1,3 +1,56 @@
-export class SlidingWindowRateLimiter {
+let _rateLimiter: SlidingWindowRateLimiter;
 
+export type SlidingWidowRateLimiterConfig = {
+    rateValue: number, //number of requests allowed per unit time
+    rateTime: number   //unit of time in milliseconds
+}
+
+export class SlidingWindowRateLimiter {
+    public rateLimitValue: number;
+    public rateLimitTime: number;
+    private prevTimeStamp: number;
+    private initialTimeStamp: number;
+    private currLimit: number;
+
+
+    private constructor(config: SlidingWidowRateLimiterConfig) {
+        this.rateLimitTime = config.rateTime;
+        this.rateLimitValue = config.rateValue;
+        this.initialTimeStamp = new Date().getTime();
+        this.prevTimeStamp = this.initialTimeStamp;
+        this.currLimit = config.rateValue;
+    }
+
+
+    public decreaseLimitVal(): number {
+        const currTimeStamp = new Date().getTime();
+        if(currTimeStamp < this.prevTimeStamp + this.rateLimitTime) {
+            if(this.currLimit >= 0) {
+                console.log(this.currLimit);
+                this.currLimit--;
+            }
+        } else {
+            const currMultiplier = Math.floor((currTimeStamp - this.initialTimeStamp))/this.rateLimitTime;
+            this.prevTimeStamp = this.initialTimeStamp + currMultiplier * this.rateLimitTime;
+            console.log(this.prevTimeStamp);
+            this.currLimit = this.rateLimitValue - 1;
+        }
+        return this.currLimit;
+    }
+
+
+    public static getInstance() {
+        if(_rateLimiter === null || _rateLimiter === undefined) {
+            throw new Error("Rate Limiter not initialized!");
+        }
+        return _rateLimiter;
+    }
+
+
+    public static initializeRateLimiter(config: SlidingWidowRateLimiterConfig): SlidingWindowRateLimiter {
+        if(_rateLimiter === null || _rateLimiter === undefined) {
+            _rateLimiter =  new SlidingWindowRateLimiter(config);
+        }
+        return _rateLimiter;
+    }
 }
